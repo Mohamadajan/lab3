4300ba37c95f3885e1ab07b89e6883b48ebeed01
MohammadAzeedin <>
2021-05-27 13:05:23 +0000

    add js file

---
+++ javascript/app.js
@@ -0,0 +1,15 @@
+var rm = "realmadrid";
+var userName = prompt("Whats your name ?")
+alert("welcome to our websit " + userName )
+var Age = prompt("How old are you?")
+
+var FavClub = prompt("What is your fav Club from most teams won ?");
+
+if(FavClub === rm){
+document.write("<p> Hala Madrid </p>")}
+else 
+if(FavClub === "barcelona") {
+document.write("<p> Visca Barca </p>") }
+
+
+
